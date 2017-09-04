38e9ced31c48b5e43cafb90a1c22b254a6ac9fd0
AlexHCC <alexander06088@yahoo.com>
2017-09-04 15:40:43 +0300

    Window display

--- main.cpp
+++ main.cpp
@@ -6,8 +6,24 @@
 
 using namespace std;
 
+//Variables
+const int WIDTH = 400, HEIGHT = 400;
+sf::RenderWindow window;
+
 int main(int argc, char** argv) {
     cout << "Overpopulation" << endl;
+    
+    window.create(sf::VideoMode(WIDTH, HEIGHT), "Overpopulation", sf::Style::Titlebar | sf::Style::Close);
+    
+    sf::Event event;
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color(30, 30, 30));
+        window.display();
+    }
     return 0;
 }
 
