4d6c9a321603b13cead7dae22171ae783b2f6438
AlexHCC <alexander06088@yahoo.com>
2017-09-04 15:34:28 +0300

    Project setup

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <thread>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "Overpopulation" << endl;
+    return 0;
+}
+
